f75273d0ac07dbe84678c8512c71603379315974
TagaevRenat <renattagaev0086@gmail.com>
2022-08-31 10:33:33 +0300

    code refactored

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,74 +3,69 @@ const chalk = require('chalk');
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class MyLogger extends ConsoleLogger {
+  static object: string;
+  static log = console.log;
+
+  checkVariable(variable: string | Array<any> | {}) {
+    if (typeof variable === 'object' && !Array.isArray(variable)) {
+      MyLogger.object = JSON.stringify(variable);
+      return MyLogger.object;
+    }
+    return variable;
+  }
+
   log(message: string, variable?: string | Array<any> | {}): void {
-    let time: string = new Date().toLocaleString();
     let context: string = this.context;
-    let object: string;
-    const log = console.log;
+    const time: string = new Date().toLocaleString();
     if (!context) {
       context = 'global';
     }
-    if (typeof variable === 'object' && !Array.isArray(variable)) {
-      object = JSON.stringify(variable);
-    }
-    log(
+    MyLogger.log(
       `[${chalk.bold(time)}], ${chalk.bold.bgGreen('LOG')} - ${chalk.green(
         message,
-      )}, Variable - ${chalk.underline(object ? object : variable)}, ${
-        'context' + ' - ' + chalk.underline(context)
-      }`,
+      )}, Variable - ${chalk.underline(
+        variable ? this.checkVariable(variable) : 'no variable send',
+      )}, ${'context' + ' - ' + chalk.underline(context)}`,
     );
   }
 
   error(error: any, variable?: string | Array<any> | {}): void {
     let time: string = new Date().toLocaleString();
     let context: string = this.context;
-    let object: string;
-    const log = console.log;
     if (!context) {
       context = 'global';
     }
-    if (typeof variable === 'object' && !Array.isArray(variable)) {
-      object = JSON.stringify(variable);
-    }
-    log(
+    MyLogger.log(
       `[${chalk.bold(time)}], ${chalk.bold.bgRed('ERROR')} - ${chalk.red(
         error.message,
-      )}, Variable - ${chalk.underline(object ? object : variable)}, ${
-        'context' + ' - ' + chalk.underline(context)
-      }`,
+      )}, Variable - ${chalk.underline(
+        variable ? this.checkVariable(variable) : 'no variable send',
+      )}, ${'context' + ' - ' + chalk.underline(context)}`,
     );
   }
 
-  warn(message: any, variable?: string | Array<any> | {}): void {
+  warn(message: string, variable?: string | Array<any> | {}): void {
     let time: string = new Date().toLocaleString();
     let context: string = this.context;
-    let object: string;
-    const log = console.log;
     if (!context) {
       context = 'global';
     }
-    if (typeof variable === 'object' && !Array.isArray(variable)) {
-      object = JSON.stringify(variable);
-    }
-    log(
+    MyLogger.log(
       `[${chalk.bold(time)}], ${chalk.bold.bgYellow('LOG')} - ${chalk.yellow(
         message,
-      )}, Variable - ${object ? object : variable}, ${
-        'context' + ' - ' + chalk.underline(context)
-      }`,
+      )}, Variable - ${chalk.underline(
+        variable ? this.checkVariable(variable) : 'no variable send',
+      )}, ${'context' + ' - ' + chalk.underline(context)}`,
     );
   }
 
   debug(error: any): void {
     let time: string = new Date().toLocaleString();
     let context: string = this.context;
-    const log = console.log;
     if (!context) {
       context = 'global';
     }
-    log(`[${chalk.bold(time)}], ${chalk.bold.bgCyan('DEBUG')} -  `);
+    MyLogger.log(`[${chalk.bold(time)}], ${chalk.bold.bgCyan('DEBUG')} -  `);
     console.trace();
   }
 }
--- src/tmdb_parser/parser.service.ts
+++ src/tmdb_parser/parser.service.ts
@@ -11,7 +11,7 @@ export class ParserService {
   async getFilmInfo(): Promise<void> {
     try {
       const response = await axios.get(
-        `https://api.themviedb.org/3/movie/475557?api_key=b40bcd1b7a69127917daf2a39a52c832&language=en-US`,
+        `https://api.themoiedb.org/3/movie/475557?api_key=b40bcd1b7a69127917daf2a39a52c832&language=en-US`,
       );
       interface Film {
         title: string;
@@ -23,10 +23,10 @@ export class ParserService {
       if (response.statusText !== 'OK') {
         throw new Error(`response status - ${response.status}`);
       }
-      this.myLogger.log('Log message', { name: 'Renat' });
+      this.myLogger.log('Log message');
     } catch (error) {
-      this.myLogger.warn('warn message', { name: 'Renat' });
-      this.myLogger.error(error, { name: 'Renat' });
+      this.myLogger.warn('warn message');
+      this.myLogger.error(error, [1, 2, 3]);
       this.myLogger.debug(error);
     }
   }
